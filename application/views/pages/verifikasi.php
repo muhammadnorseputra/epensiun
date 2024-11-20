64643e626bf819cc741f136115a12be85afc7efd
Putrabungsu6 <muhammadnorseputra@gmail.com>
2024-11-20 19:28:50 +0800

    add: menambahkan alert pada halaman verifikasi admin

--- application/views/pages/verifikasi.php
+++ application/views/pages/verifikasi.php
@@ -15,6 +15,15 @@
             </div>
             <!-- table  -->
             <div class="table-responsive p-4">
+                <div class="alert alert-warning d-flex align-items-start" role="alert">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-info-circle-fill me-2" viewBox="0 0 16 16">
+                        <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z" />
+                    </svg>
+                    <div>
+                        <strong>Perhatian!</strong> <br>
+                        Jangan lupa untuk mengarsipkan usulan, jika SK sudah diserahkan atau diterima yang bersangkutan.
+                    </div>
+                </div>
                 <table id="table-verifikasi" class="table table-hover table-nowrap mb-0">
                     <thead class="bg-light">
                         <tr>

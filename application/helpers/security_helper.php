7c1f61935528dc06cdfd19c6a334c81a35df47f8
Putrabungsu6 <muhammadnorseputra@gmail.com>
2025-05-14 23:46:58 +0800

    update: dashboard

--- application/helpers/security_helper.php
+++ application/helpers/security_helper.php
@@ -1,110 +1,109 @@
 <?php if (!defined("BASEPATH")) exit("No direct script access allowed");
-if ( ! function_exists('do_hash'))
+if (! function_exists('do_hash')) {
+  function do_hash($str, $type = 'sha1')
   {
-      function do_hash($str, $type = 'sha1')
-      {
-          if ($type == 'sha1')
-          {
-              return sha1($str);
-          }
-          else
-          {
-              return md5($str);
-          }
-      }
- }
-function encrypt_url($string) {
+    if ($type == 'sha1') {
+      return sha1($str);
+    } else {
+      return md5($str);
+    }
+  }
+}
+function encrypt_url($string)
+{
 
-    $output = false;
-    /*
+  $output = false;
+  /*
     * read security.ini file & get encryption_key | iv | encryption_mechanism value for generating encryption code
-    */        
-    $security       = parse_ini_file("security.ini");
-    $secret_key     = $security["encryption_key"];
-    $secret_iv      = $security["iv"];
-    $encrypt_method = $security["encryption_mechanism"];
+    */
+  $security       = parse_ini_file("security.ini");
+  $secret_key     = $security["encryption_key"];
+  $secret_iv      = $security["iv"];
+  $encrypt_method = $security["encryption_mechanism"];
 
-    // hash
-    $key    = hash("sha256", $secret_key);
+  // hash
+  $key    = hash("sha256", $secret_key);
 
-    // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
-    $iv     = substr(hash("sha256", $secret_iv), 0, 16);
+  // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
+  $iv     = substr(hash("sha256", $secret_iv), 0, 16);
 
-    //do the encryption given text/string/number
-    $result = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
-    $output = base64_encode($result);
-    return $output;
+  //do the encryption given text/string/number
+  $result = openssl_encrypt($string, $encrypt_method, $key, 0, $iv);
+  $output = base64_encode($result);
+  return $output;
 }
 
 
 
-function decrypt_url($string) {
+function decrypt_url($string)
+{
 
-    $output = false;
-    /*
+  $output = false;
+  /*
     * read security.ini file & get encryption_key | iv | encryption_mechanism value for generating encryption code
     */
 
-    $security       = parse_ini_file("security.ini");
-    $secret_key     = $security["encryption_key"];
-    $secret_iv      = $security["iv"];
-    $encrypt_method = $security["encryption_mechanism"];
+  $security       = parse_ini_file("security.ini");
+  $secret_key     = $security["encryption_key"];
+  $secret_iv      = $security["iv"];
+  $encrypt_method = $security["encryption_mechanism"];
 
-    // hash
-    $key    = hash("sha256", $secret_key);
+  // hash
+  $key    = hash("sha256", $secret_key);
 
-    // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
-    $iv = substr(hash("sha256", $secret_iv), 0, 16);
+  // iv – encrypt method AES-256-CBC expects 16 bytes – else you will get a warning
+  $iv = substr(hash("sha256", $secret_iv), 0, 16);
 
-    //do the decryption given text/string/number
+  //do the decryption given text/string/number
 
-    $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
-    return $output;
+  $output = openssl_decrypt(base64_decode($string), $encrypt_method, $key, 0, $iv);
+  return $output;
 }
 
-if ( ! function_exists('cek_session'))
+if (! function_exists('cek_session')) {
+  function cek_session()
   {
-      function cek_session()
-      {
-          $ci = get_instance();
-        //   CEK SESSION UID
-          if($ci->session->userdata('nip') === '' || !$ci->session->csrf_token):
-            return redirect(base_url('/'));
-          endif;
-      }
- }
-
- if ( ! function_exists('getSetting'))
-  {
-      function getSetting($key)
-      {
-          $ci = get_instance();
-          $db = $ci->global->getSetting(['key' => $key, 'status' => 'Y']);
-          return isset($db) ? $db : '';
-      }
- }
-
- if ( ! function_exists('privilages'))
- {
-     function privilages($priv_name = false)
-     {
-         $ci = get_instance();
-         $user_id = $ci->session->userdata('user_id');
-         $priv = $ci->users->get_privilages($user_id);
-         $priv_name = isset($priv[$priv_name]) ? $priv[$priv_name] : "N"; 
-         if(($priv != 0) && ($priv_name == 'Y')) {
-           $p = true;
-         } else {
-           $p = false;
-         }
-         return $p;
-     }
+    $ci = get_instance();
+    //   CEK SESSION UID
+    if ($ci->session->userdata('nip') === '' || !$ci->session->csrf_token):
+      return redirect(base_url('/'));
+    endif;
+  }
 }
 
-function calculatePercentage($numerator, $denominator) {
+if (! function_exists('getSetting')) {
+  function getSetting($key)
+  {
+    $ci = get_instance();
+    $db = $ci->global->getSetting(['key' => $key, 'status' => 'Y']);
+    return isset($db) ? $db : '';
+  }
+}
+
+if (! function_exists('privilages')) {
+  function privilages($priv_name = false)
+  {
+    $ci = get_instance();
+    $user_id = $ci->session->userdata('user_id');
+    $priv = $ci->users->get_privilages($user_id);
+    $priv_name = isset($priv[$priv_name]) ? $priv[$priv_name] : "N";
+    if (($priv != 0) && ($priv_name == 'Y')) {
+      $p = true;
+    } else {
+      $p = false;
+    }
+    return $p;
+  }
+}
+
+function calculatePercentage($numerator, $denominator)
+{
   // Added a check for zero denominator to prevent DivisionByZeroError
   if ($denominator == 0) {
-      return 0; // Or handle the scenario appropriately based on application logic
+    return 0; // Or handle the scenario appropriately based on application logic
   }
-  return ($numerator / $denominator) * 100;
+  $percentage = ($numerator / $denominator) * 100;
+  // Format the percentage to 2 decimal places  
+  $formatted = number_format($percentage, 2); // hanya 2 angka di belakang koma
+  return $formatted . '.%';
 }
--- application/views/pages/dashboard.php
+++ application/views/pages/dashboard.php
@@ -13,10 +13,10 @@ $stat = json_decode($statistik);
                         <h3 class="mb-0  text-white">Dahsboard</h3>
                     </div>
                     <?php if ($this->session->userdata('level') === 'ADMIN'): ?>
-                    <div>
-                        <a href="<?= base_url('app/pensiun/buatusul') ?>" class="btn btn-white"><i class="icon-sm me-2"
-                                data-feather="plus-circle"></i>Buat Usul Pensiun</a>
-                    </div>
+                        <div>
+                            <a href="<?= base_url('app/pensiun/buatusul') ?>" class="btn btn-white"><i class="icon-sm me-2"
+                                    data-feather="plus-circle"></i>Buat Usul Pensiun</a>
+                        </div>
                     <?php endif; ?>
                 </div>
             </div>
@@ -136,195 +136,195 @@ $stat = json_decode($statistik);
         </div>
     </div>
     <?php if ($this->session->userdata('level') === 'ADMIN' || $this->session->userdata('level') === 'USER'): ?>
-    <!-- row  -->
-    <div class="row mb-6">
-        <div class="col-xl-5 col-lg-12 col-md-12 col-12 mb-6 mb-xl-0">
-            <!-- card  -->
-            <div class="card h-100">
-                <div class="card-header pt-4 pb-2">
-                    <div class="d-flex align-items-center
+        <!-- row  -->
+        <div class="row mb-6">
+            <div class="col-xl-5 col-lg-12 col-md-12 col-12 mb-6 mb-xl-0">
+                <!-- card  -->
+                <div class="card h-100">
+                    <div class="card-header pt-4 pb-2">
+                        <div class="d-flex align-items-center
                     justify-content-center">
-                        <div>
-                            <h4>Trend Berdasarkan Jenis Pensiun </h4>
+                            <div>
+                                <h4>Trend Berdasarkan Jenis Pensiun </h4>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <!-- card body  -->
-                <div class="card-body">
+                    <!-- card body  -->
+                    <div class="card-body">
 
-                    <!-- chart  -->
-                    <div class="mb-8">
-                        <div id="PensiunChart" data-bup="<?= $charts['bup'] ?>" data-jadu="<?= $charts['jadu'] ?>"
-                            data-aps="<?= $charts['aps'] ?>" data-udzur="<?= $charts['udzur'] ?>"
-                            data-mpp="<?= $charts['mpp'] ?>"></div>
-                    </div>
-                    <!-- icon with content  -->
-                    <div class="d-flex align-items-center justify-content-around">
-                        <div class="text-center">
-                            <i class="icon-sm text-success" data-feather="activity"></i>
-                            <h1 class="mt-3  mb-1 fw-bold"><?= $charts['bup'] ?></h1>
-                            <p>BUP</p>
+                        <!-- chart  -->
+                        <div class="mb-8">
+                            <div id="PensiunChart" data-bup="<?= $charts['bup'] ?>" data-jadu="<?= $charts['jadu'] ?>"
+                                data-aps="<?= $charts['aps'] ?>" data-udzur="<?= $charts['udzur'] ?>"
+                                data-mpp="<?= $charts['mpp'] ?>"></div>
                         </div>
-                        <div class="text-center">
-                            <i class="icon-sm text-warning" data-feather="activity"></i>
-                            <h1 class="mt-3  mb-1 fw-bold"><?= $charts['jadu'] ?></h1>
-                            <p>JANDA/DUDA</p>
-                        </div>
-                        <div class="text-center">
-                            <i class="icon-sm text-secondary" data-feather="activity"></i>
-                            <h1 class="mt-3  mb-1 fw-bold"><?= $charts['aps'] ?></h1>
-                            <p>APS</p>
-                        </div>
-                        <div class="text-center">
-                            <i class="icon-sm text-danger" data-feather="activity"></i>
-                            <h1 class="mt-3  mb-1 fw-bold"><?= $charts['udzur'] ?></h1>
-                            <p>UDZUR</p>
-                        </div>
-                        <div class="text-center">
-                            <i class="icon-sm text-info" data-feather="activity"></i>
-                            <h1 class="mt-3  mb-1 fw-bold"><?= $charts['mpp'] ?></h1>
-                            <p>MPP</p>
+                        <!-- icon with content  -->
+                        <div class="d-flex align-items-center justify-content-around">
+                            <div class="text-center">
+                                <i class="icon-sm text-success" data-feather="activity"></i>
+                                <h1 class="mt-3  mb-1 fw-bold"><?= $charts['bup'] ?></h1>
+                                <p>BUP</p>
+                            </div>
+                            <div class="text-center">
+                                <i class="icon-sm text-warning" data-feather="activity"></i>
+                                <h1 class="mt-3  mb-1 fw-bold"><?= $charts['jadu'] ?></h1>
+                                <p>JANDA/DUDA</p>
+                            </div>
+                            <div class="text-center">
+                                <i class="icon-sm text-secondary" data-feather="activity"></i>
+                                <h1 class="mt-3  mb-1 fw-bold"><?= $charts['aps'] ?></h1>
+                                <p>APS</p>
+                            </div>
+                            <div class="text-center">
+                                <i class="icon-sm text-danger" data-feather="activity"></i>
+                                <h1 class="mt-3  mb-1 fw-bold"><?= $charts['udzur'] ?></h1>
+                                <p>UDZUR</p>
+                            </div>
+                            <div class="text-center">
+                                <i class="icon-sm text-info" data-feather="activity"></i>
+                                <h1 class="mt-3  mb-1 fw-bold"><?= $charts['mpp'] ?></h1>
+                                <p>MPP</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!-- card  -->
-        <div class="col-xl-7 col-lg-12 col-md-12 col-12">
-            <div class="card h-100">
-                <!-- card header  -->
-                <div class="card-header pt-4 pb-2">
-                    <h4>Usulan Pensiun Terbit SK</h4>
-                </div>
+            <!-- card  -->
+            <div class="col-xl-7 col-lg-12 col-md-12 col-12">
+                <div class="card h-100">
+                    <!-- card header  -->
+                    <div class="card-header pt-4 pb-2">
+                        <h4>Usulan Pensiun Terbit SK</h4>
+                    </div>
 
-                <div class="card-body pt-0 px-0">
-                    <!-- table  -->
-                    <div class="table-responsive">
-                        <table class="table text-nowrap">
-                            <thead class="table-light">
-                                <tr>
-                                    <th>Pensiun ASN</th>
-                                    <th>Nomor & Tanggal SK</th>
-                                    <th>Tanggal Diserahkan</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php
+                    <div class="card-body pt-0 px-0">
+                        <!-- table  -->
+                        <div class="table-responsive">
+                            <table class="table text-nowrap">
+                                <thead class="table-light">
+                                    <tr>
+                                        <th>Pensiun ASN</th>
+                                        <th>Nomor & Tanggal SK</th>
+                                        <th>Tanggal Diserahkan</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
                                     $db = $this->inbox->TopUsulanPensiunByUnkerId();
                                     if ($db->num_rows() > 0) :
                                         foreach ($db->result() as $r) :
 
                                             $arsip_date = $r->arsip_at !== null ? date_indo(substr($r->arsip_at, 0, 10)) : '<span class="text-danger bg-light p-2 rounded ">SK Belum diserahkan</span>';
                                     ?>
-                                <tr>
-                                    <td class="align-middle">
-                                        <div class="d-flex align-items-start">
-                                            <div>
-                                                <img src="<?= $r->url_photo ?>" alt="<?= $r->nama ?>"
-                                                    class="avatar-md avatar rounded-circle">
-                                            </div>
-                                            <div class="ms-3 lh-1">
-                                                <h5 class="mb-1 text-dark"><?= $r->nama ?></h5>
-                                                <p class=" fw-bold text-dark"><?= $r->nip ?></p>
-                                                <p class="mb-0"><?= $r->jenis_pensiun ?></p>
-                                            </div>
-                                        </div>
-                                    </td>
-                                    <td class="align-middle"><span class="fw-bold"><?= $r->nomor_sk ?> </span><br>
-                                        <?= date_indo($r->tanggal_sk) ?></td>
-                                    <td class="align-middle"><?= $arsip_date ?></td>
-                                </tr>
-                                <?php
+                                            <tr>
+                                                <td class="align-middle">
+                                                    <div class="d-flex align-items-start">
+                                                        <div>
+                                                            <img src="<?= base_url("template/assets/images/avatar/avatar.jpg") ?>" alt="<?= $r->nama ?>"
+                                                                class="avatar-md avatar rounded-circle">
+                                                        </div>
+                                                        <div class="ms-3 lh-1">
+                                                            <h5 class="mb-1 text-dark"><?= $r->nama ?></h5>
+                                                            <p class=" fw-bold text-dark"><?= $r->nip ?></p>
+                                                            <p class="mb-0"><?= $r->jenis_pensiun ?></p>
+                                                        </div>
+                                                    </div>
+                                                </td>
+                                                <td class="align-middle"><span class="fw-bold"><?= $r->nomor_sk ?> </span><br>
+                                                    <?= date_indo($r->tanggal_sk) ?></td>
+                                                <td class="align-middle"><?= $arsip_date ?></td>
+                                            </tr>
+                                        <?php
                                         endforeach;
                                     else :
                                         ?>
 
-                                <tr>
-                                    <td colspan="4" class="text-center">
-                                        Top Data Usulan SK Telah Terbit Tidak Tersedia
-                                    </td>
-                                </tr>
-                                <?php
+                                        <tr>
+                                            <td colspan="4" class="text-center">
+                                                Top Data Usulan SK Telah Terbit Tidak Tersedia
+                                            </td>
+                                        </tr>
+                                    <?php
                                     endif;
                                     ?>
-                            </tbody>
-                        </table>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
-                </div>
-                <div class="card-footer text-muted">
-                    <div class="text-center">
-                        <a href="<?= base_url('/app/inbox/usul') ?>">Lihat Semua Usulan</a>
+                    <div class="card-footer text-muted">
+                        <div class="text-center">
+                            <a href="<?= base_url('/app/inbox/usul') ?>">Lihat Semua Usulan</a>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
 
     <?php endif; ?>
     <?php if ($this->session->userdata('level') === 'ADMIN' || $this->session->userdata('level') === 'USER'): ?>
-    <!-- row -->
-    <div class="row mb-6">
-        <div class="col-md-12">
-            <div class="card h-100">
-                <div class="card-header pt-4 pb-2">
-                    <h4>Trend Kesalahan (<?= $charts['total_kesalahan'] ?>)</h4>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-12 col-md-5">
-                            <div id="PensiunChartByKesalahan" data-tms="<?= $charts['tms'] ?>"
-                                data-btl="<?= $charts['btl'] ?>"></div>
-                        </div>
-                        <div class="col-12 col-md-7">
-                            <div class="table-responsive">
-                                <table class="table text-nowrap">
-                                    <thead class="table-light">
-                                        <tr>
-                                            <th width="5">No.</th>
-                                            <th width="20">Status</th>
-                                            <th>Catatan</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <?php $no = 1;
+        <!-- row -->
+        <div class="row mb-6">
+            <div class="col-md-12">
+                <div class="card h-100">
+                    <div class="card-header pt-4 pb-2">
+                        <h4>Trend Kesalahan (<?= $charts['total_kesalahan'] ?>)</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-12 col-md-5">
+                                <div id="PensiunChartByKesalahan" data-tms="<?= $charts['tms'] ?>"
+                                    data-btl="<?= $charts['btl'] ?>"></div>
+                            </div>
+                            <div class="col-12 col-md-7">
+                                <div class="table-responsive">
+                                    <table class="table text-nowrap">
+                                        <thead class="table-light">
+                                            <tr>
+                                                <th width="5">No.</th>
+                                                <th width="20">Status</th>
+                                                <th>Catatan</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php $no = 1;
                                             foreach ($charts['catatan'] as $r):  ?>
-                                        <tr>
-                                            <th><?= $no ?></th>
-                                            <th><?= $r->is_status ?></th>
-                                            <th><?= $r->catatan ?></th>
-                                        </tr>
-                                        <?php $no++;
+                                                <tr>
+                                                    <th><?= $no ?></th>
+                                                    <th><?= $r->is_status ?></th>
+                                                    <th><?= $r->catatan ?></th>
+                                                </tr>
+                                            <?php $no++;
                                             endforeach; ?>
-                                    </tbody>
-                                </table>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <!-- row -->
-    <div class="row mb-6">
-        <div class="col-md-12">
-            <div class="card h-100">
-                <div class="card-header pt-4 pb-2">
-                    <h4>Trend Usulan Berdasarkan Periode</h4>
-                </div>
-                <div class="card-body">
-                    <div class="col-md-12">
+        <!-- row -->
+        <div class="row mb-6">
+            <div class="col-md-12">
+                <div class="card h-100">
+                    <div class="card-header pt-4 pb-2">
+                        <h4>Trend Usulan Berdasarkan Periode</h4>
+                    </div>
+                    <div class="card-body">
+                        <div class="col-md-12">
 
-                        <select class="form-select" id="single-select-clear-field" data-placeholder="Pilih Unit Organisasi/SKPD/SOPD">
-                            <option></option>
-                            <?php foreach(json_decode($listunor) as $unor): ?>
-                                <option value="<?= $unor->id_unit_kerja ?>"><?= $unor->nama_unit_kerja ?></option>
+                            <select class="form-select" id="single-select-clear-field" data-placeholder="Pilih Unit Organisasi/SKPD/SOPD">
+                                <option></option>
+                                <?php foreach (json_decode($listunor) as $unor): ?>
+                                    <option value="<?= $unor->id_unit_kerja ?>"><?= $unor->nama_unit_kerja ?></option>
                                 <?php endforeach; ?>
                             </select>
                         </div>
-                    <div id="PensiunChartUsulanPeriode" data-unorid="<?= $this->session->userdata('unker_id') ?>"></div>
+                        <div id="PensiunChartUsulanPeriode" data-unorid="<?= $this->session->userdata('unker_id') ?>"></div>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
     <?php endif; ?>
